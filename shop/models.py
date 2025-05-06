90663b9134dbb1532d81c9d6a3b9e9f0e961d945
cristivalea <160530571+cristivalea@users.noreply.github.com>
2025-05-06 15:17:32 +0300

    Populare tabela tablete si crerea modelului pentru tableta

--- shop/models.py
+++ shop/models.py
@@ -26,3 +26,34 @@ class Laptop(models.Model):
 
     def __str__(self):
         return f"{self.brand} {self.model} - {self.pret} RON"
+
+
+class Tableta(models.Model):
+    serietableta = models.CharField(max_length=50, primary_key=True, db_column='SerieTableta')
+    sistem_operare = models.CharField(max_length=50, db_column='SistemOperare')
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    capacitate_ram = models.CharField(max_length=50, db_column='CapacitateRAM')
+    capacitate_memorie = models.CharField(max_length=50, db_column='CapacitateMemorie')
+    culoare = models.CharField(max_length=50)
+    lungime = models.IntegerField()
+    latime = models.IntegerField()
+    unitati_masura = models.CharField(max_length=20, db_column='UnitatiMasura')
+    grosime = models.IntegerField()
+    greutate = models.IntegerField()
+    capacitate_acumulator = models.IntegerField(db_column='CapacitateAcumulator')
+    rezolutie = models.IntegerField()
+    diagonala = models.IntegerField()
+    conectivitate = models.CharField(max_length=20)
+    model_procesor = models.CharField(max_length=50, db_column='ModelProcesor')
+    pret = models.IntegerField(null=True)
+    nota_produs = models.FloatField(null=True, db_column='NotaProdus')
+    disponibilitate = models.CharField(max_length=100, null=True)
+    optiune_livrare = models.CharField(max_length=100, null=True, db_column='OptiuneLivrare')
+
+    class Meta:
+        db_table = 'tablete'
+
+    def __str__(self):
+        return f"{self.brand} {self.model} - {self.pret} RON"
+
